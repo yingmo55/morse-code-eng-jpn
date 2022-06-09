97e5af945b184885761fd411d2333536634e34a7
Ying <97061798+yingmo55@users.noreply.github.com>
2022-06-09 18:02:43 -0400

    Add warning if invalid character is detected

--- src/Components/LetterToMorseCode/LetterToMorse.ts
+++ src/Components/LetterToMorseCode/LetterToMorse.ts
@@ -1,19 +1,25 @@
-import { morseDictionary } from "../morseCode/morseCode";
+import { morseAlphabetDictionary, morseNumberDictionary,morseSymbolDictionary } from "../morseCode/morseCode";
 
 const letterToMorse = (input: string): string => {
-    const DictionaryKey: string[] = Object.keys(morseDictionary);
+    const engMorseDictionary = {
+        ...morseAlphabetDictionary,
+        ...morseNumberDictionary,
+        ...morseSymbolDictionary}
     input = input.toUpperCase()
     let newString: string ='';
 
     // warning: Function declared in a loop contains unsafe references to variable(s) 'newString'
     for (let i=0; i < input.length; i++) {
-        DictionaryKey.forEach((dict)=> {
-            if (input[i] in (morseDictionary as any)[dict]) {
-                newString += (morseDictionary as any)[dict][input[i]] + ' '
+
+            if (input[i] in engMorseDictionary) {
+                newString += engMorseDictionary[input[i]] + ' '
+            }  else {
+                newString = 'Invalid input detected.'
+                break;
             }
-        })
-    }
+
+        }
     return newString;
-}
+    }
 
 export default letterToMorse;
