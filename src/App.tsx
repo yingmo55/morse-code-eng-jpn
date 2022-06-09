d469b391afa701a6540e790b681e537338666173
Ying <97061798+yingmo55@users.noreply.github.com>
2022-06-09 12:47:30 -0400

    Adjust function. Add props and types for props
    - Add readOnly and label props

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import TextInput from './Components/TextInput/TextInput';
 
 function App() {
+
+  // const [ ]
+
   return (
     <div className="App">
-      <header className="App-header">
-      <TextInput />
+      <header>
+        <h1>Morse Code Translator</h1>
       </header>
+      <TextInput label={'Input'} readOnly={false} />
+      <TextInput label={'Output'} readOnly={true} />
     </div>
   );
 }
--- src/Components/TextInput/TextInput.tsx
+++ src/Components/TextInput/TextInput.tsx
@@ -1,17 +1,37 @@
 import React, {useState} from 'react';
 import { TextField } from '@mui/material';
 
-const  TextInput = () => {
+interface Props {
+  label: string;
+  readOnly: boolean;
+  placeholder?: string;
+  // output?: string;
+}
+
+const  TextInput = (props: Props) => {
     const [input, setInput] = useState<string>('');
+    const {label, readOnly} = props;
+
+    const output = 'test output'
+
+    const handleTextInput = (e:any) => {
+      setInput(e.target.value)
+    }
 
     return (
         <TextField
         id="outlined-multiline-static"
-        label="Multiline"
+        label={label}
         multiline
         fullWidth
         rows={4}
-        defaultValue="type something here (. as dot, - as dash. empty space for space)"
+        InputProps={{
+          readOnly: readOnly,
+        }}
+        margin="normal"
+        placeholder="type something here (. as dot, - as dash. empty space for space)"
+        value={!readOnly ? input: output}
+        onChange={handleTextInput}
       />
     )
 }
