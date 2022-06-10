50f5cd056207fc3780d7088519e57c66fb765935
Ying <97061798+yingmo55@users.noreply.github.com>
2022-06-10 15:59:05 -0400

    Add condition to forming a new string
    - If the letter is the last letter of the string, it won't add an empty
      space afterward.

--- src/Components/LetterToMorseCode/LetterToMorse.ts
+++ src/Components/LetterToMorseCode/LetterToMorse.ts
@@ -12,12 +12,12 @@ const letterToMorse = (input: string): string => {
     for (let i=0; i < input.length; i++) {
 
             if (input[i] in engMorseDictionary) {
-                newString += engMorseDictionary[input[i]] + ' '
+                newString += i === (input.length - 1) ? 
+                            engMorseDictionary[input[i]] : engMorseDictionary[input[i]] + ' '
             }  else {
                 newString = 'Invalid input detected.'
                 break;
             }
-
         }
     return newString;
     }
