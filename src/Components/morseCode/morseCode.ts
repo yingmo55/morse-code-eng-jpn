813eb212ec3c2b426da8a216902e62ad62da6b6b
Ying <97061798+yingmo55@users.noreply.github.com>
2022-06-10 17:35:15 -0400

    Add additional Japanese kana arrays

--- src/Components/morseCode/morseCode.ts
+++ src/Components/morseCode/morseCode.ts
@@ -125,4 +125,10 @@ const morseJpnSymbolDictionary = {
     '、': '.-.-.-'
 }
 
+const sutegana: string[] = ['ャ', 'ュ', 'ョ', 'ッ', 'ァ', 'ィ', 'ゥ', 'ェ', 'ォ'];
+
+const dakuon: string[] = ['ガ', 'ギ', 'グ', 'ゲ', 'ゴ', 'ダ', 'ヂ', 'ヅ', 'デ', 'ド', 'ザ', 'ジ', 'ズ', 'ゼ', 'ゾ', 'バ', 'ビ', 'ブ', 'ベ', 'ボ', 'ヴ']
+
+const hanDakuon: string[] = [ 'パ', 'ピ', 'プ', 'ペ', 'ポ']
+
 export { morseAlphabetDictionary, morseNumberDictionary,morseSymbolDictionary, morseJpnDictionary, morseJpnSymbolDictionary }
